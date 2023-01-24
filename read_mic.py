496c684a34e76d4c4ec1561f1f44b875d53ab821
Nick Werblun <nickwerblun@gmail.com>
2023-01-24 14:44:03 -0800

    Maybe it's GUI time?

--- read_mic.py
+++ read_mic.py
@@ -1,5 +1,6 @@
 import pyaudio
 import time
+import os
 import numpy as np
 from scipy import signal
 #import tensorflow.compat.v1 as tf
@@ -7,6 +8,7 @@ from tensorflow import keras
 from matplotlib.colors import LogNorm
 import matplotlib.cm as cm
 from PIL import Image
+from multiprocessing import Process, Value
 
 pa = pyaudio.PyAudio()
 model = keras.models.load_model("C:\\Users\\NWerblun\\Desktop\\selective_voice_filter\\model.h5")
@@ -14,31 +16,21 @@ sample_format = pyaudio.paInt16
 channels = 1
 fs = 16000
 feedthrough_chunk_size = 2048
-
-dBFS = 0
 PREDICTION = -float("inf")
 #Compute once and reuse
 time_chunks = int((16000/256) + ((16000-128)/256))
 #Two bytes per sample, so need double the length. 1 second + 1 buffer
 QUEUE_fs = bytearray(88200+4096)
-
-
-# Load keras model as a TF v1 graph with no eager execution
-# see here: https://ksingh7.medium.com/part-iii-convert-keras-model-to-tensorflow-frozen-graph-model-a6aa6b1aaeee
-# sess=tf.InteractiveSession()
-# frozen_graph="./frozen_model.pb"
-# with tf.gfile.GFile(frozen_graph, "rb") as f:
-#       graph_def = tf.GraphDef()
-#       graph_def.ParseFromString(f.read())
-# sess.graph.as_default()
-# tf.import_graph_def(graph_def)
-# input_tensor = sess.graph.get_tensor_by_name("x:0")
-# output_tensor = sess.graph.get_tensor_by_name("Identity:0")
+MAX_MOMENTUM = 15
+last_n_pred = Value('i', -5)
+last_process_time = Value('d', 0.0)
 
 def callback_both(in_data, frame_count, time_info, status_flags):
     global QUEUE_fs
     global PREDICTION
-    # p0 = time.perf_counter_ns()
+    global last_n_pred
+    global last_process_time
+    p0 = time.perf_counter_ns()
     #Cannot do this in one line with byte arrays
     QUEUE_fs = QUEUE_fs[4096:]
     QUEUE_fs.extend(in_data)
@@ -68,7 +60,7 @@ def callback_both(in_data, frame_count, time_info, status_flags):
     PREDICTION = model(rgb, training=False)
     # PREDICTION = sess.run(output_tensor, {'x:0': rgb})
     # p10 = time.perf_counter_ns()
-    sig = tf.keras.activations.sigmoid(PREDICTION)
+    sig = keras.activations.sigmoid(PREDICTION)
     # p11 = time.perf_counter_ns()
     # print("\n"*4+"PRED: {}\nSIGM: {}".format(PREDICTION, sig))
     # print("Queue ops: {:2.2f}ms".format(1e-6 * (p1-p0)))
@@ -83,15 +75,36 @@ def callback_both(in_data, frame_count, time_info, status_flags):
     # print("Predict: {:2.2f}ms".format(1e-6 * (p10-p9)))
     # print("Sigmoid: {:2.2f}ms".format(1e-6 * (p11-p10)))
     if sig >= 0.97:
-        # p12 = time.perf_counter_ns()
+        last_n_pred.value = last_n_pred.value + 1 if last_n_pred.value < MAX_MOMENTUM else MAX_MOMENTUM
+        p12 = time.perf_counter_ns()
+        last_process_time.value = p12-p0
         # print("Compare: {:2.2f}ms".format(1e-6 * (p12-p11)))
         # print("Total: {:2.2f}ms".format(1e-6 * (p12-p0)))
-        return (in_data, pyaudio.paContinue)
+        if last_n_pred.value > 0:
+            return (in_data, pyaudio.paContinue)
+        else:
+            return (np.zeros(4096,).tobytes(), pyaudio.paContinue)
     else:
-        # p12 = time.perf_counter_ns()
+        last_n_pred.value = last_n_pred.value - 1 if last_n_pred.value > -MAX_MOMENTUM else -MAX_MOMENTUM
+        p12 = time.perf_counter_ns()
+        last_process_time.value = p12-p0
         # print("Compare: {}ms".format(1e-6 * (p12-p11)))
         # print("Total with prints: {:2.2f}ms".format(1e-6 * (p12-p0)))
-        return (np.zeros(4096,).tobytes(), pyaudio.paContinue)
+        if last_n_pred.value <= 0:
+            return (np.zeros(4096,).tobytes(), pyaudio.paContinue)
+        else:
+            return (in_data, pyaudio.paContinue)
+
+def monitor(stop, last_n_pred, last_process_time):
+    while True:
+        if stop.value:
+            break
+        pass
+        os.system("cls")
+        print("Last process time: {}ms".format(1e-6*last_process_time.value))
+        print("Current momentum {}".format(last_n_pred.value))
+        print("Active? {}".format(last_n_pred.value>0))
+        time.sleep(0.1)
 
 """
 Changes when restarting computer
@@ -100,20 +113,26 @@ Write to VB cable input, and it gets carried to VB cable output. Use VB cable ou
 'defaultLowInputLatency': 0.09, 'defaultLowOutputLatency': 0.09, 'defaultHighInputLatency': 0.18,
 'defaultHighOutputLatency': 0.18, 'defaultSampleRate': 44100.0}
 """
-stream = pa.open(format=sample_format,
-                    channels=channels,
-                    rate=44100,
-                    frames_per_buffer=feedthrough_chunk_size,
-                    input=True,
-                    output=True,
-                    output_device_index=11,
-                    stream_callback=callback_both)
 
-try:
-    while True:
-        time.sleep(0.6)
-except KeyboardInterrupt:
-    print("Killing Process")
-    stream.stop_stream()
-    stream.close()
-    pa.terminate()
+if __name__ == "__main__":
+    stream = pa.open(format=sample_format,
+                        channels=channels,
+                        rate=44100,
+                        frames_per_buffer=feedthrough_chunk_size,
+                        input=True,
+                        output=True,
+                        output_device_index=8,
+                        stream_callback=callback_both)
+
+    try:
+        stop = Value('i', 0)
+        mon = Process(target=monitor, args=(stop, last_n_pred, last_process_time))
+        mon.start()
+        while True:
+            time.sleep(0.6)
+    except KeyboardInterrupt:
+        print("Killing Process")
+        stop.value = 1
+        stream.stop_stream()
+        stream.close()
+        pa.terminate()
